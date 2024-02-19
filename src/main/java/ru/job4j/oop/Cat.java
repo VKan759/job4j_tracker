db01be5b90d8e32fa278cb3e00bdf2e87000ec9d
Vyacheslav Kan <vkan745@gmail.com>
2024-02-19 13:27:21 +0600

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,25 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println("There are " + this.name + '\'' + "s food - " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat cat = new Cat();
+        cat.giveNick("Vasya");
+        cat.eat("Kotleta");
+        cat.show();
     }
 }
